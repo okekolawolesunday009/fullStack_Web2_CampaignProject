a7324ec6497d03ddbc8c6f3df9c68415c7f5dcd4
OkeKolawoleSunday009 <OkeKolawoleSunday@gmail.com>
2024-09-19 12:13:59 +0100

    udate cors for render url

--- node/src/config.js/db.js
+++ node/src/config.js/db.js
@@ -6,17 +6,19 @@ const connectDB = () => {
     const host = process.env.HOST;
     const password = process.env.PASSWORD;
     const db = process.env.DB;
-    const port = process.env.PORT || 27017; // Default port if not set
+    const port = process.env.PORT ; // Default port if not set
 
     // Construct the MongoDB connection URL
+    // mongodb://u0hj6ntji8cedro2v6ho:IHy742BoI3R1lKwJd2I@b2bprg2skkjwelz3wmk7-mongodb.services.clever-cloud.com:2196/b2bprg2skkjwelz3wmk7
     const dburl = `mongodb://${username}:${password}@${host}:${port}/${db}`;
 
     // Use the environment variable URL if it exists; otherwise, use the constructed URL
     const finalDBURL = DBURL || dburl;
 
-    mongoose.connect(finalDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect(dburl, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log(`Server is up and running on port ${process.env.PORT}`);
+        console.log(`Server is up and running on port ${process.env.PORT}
+            and ${dburl}`);
     })
     .catch((err) => {
         console.error("Error connecting:", err);
